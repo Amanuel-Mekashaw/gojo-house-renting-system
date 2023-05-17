b2def750b6bed77471926b6a88aeeb986e1345a7
Amanuel <amanuelmekashaw29@gmail.com>
2023-05-17 20:49:50 +0300

    custom signin, login and Logout implemented

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import LoginModal from "../Modals/LoginModal";
+import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="
+                hidden 
+                md:block 
+                text-sm 
+                font-semibold 
+                py-3 
+                px-4
+                rounded-full
+                hover:bg-neutral-100
+                cursor-pointer"
+        >
+          Register Your House
+        </div>
+
+        <div
+          onClick={toggleOpen}
+          className="
+                p-4
+                md:py-1
+                md:px-2
+                border-[1px]
+                border-neutral-200
+                flex
+                flex-row
+                items-center
+                gap-3
+                rounded-full
+                cursor-pointer
+                hover:shadow-md
+                transtion
+            "
+        >
+          {/* Hamburger Menu */}
+          <AiOutlineMenu />
+          {/* Avatar image holder */}
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+
+      {/* TODO Is open condition Duration : 31:33 */}
+
+      {isOpen && (
+        <div
+          className="
+                absolute
+                rounded-xl
+                shadow-md
+                w-[40vw]
+                md:w-3/4
+                bg-white
+                overflow-hidden
+                right-0
+                top-12
+                text-sm
+
+            "
+        >
+          <div className="flex flex-col cursor-pointer">
+            {/* Signin conditional navbar render */}
+
+            {currentUser ? (
+              <>
+                {/* TODO Replace Menu item */}
+                <MenuItem onClick={() => {}} label="My Trips" />
+
+                {/* TODO Replace Menu item */}
+                <MenuItem onClick={() => {}} label="My Favourite" />
+
+                {/* TODO Replace Menu item */}
+                <MenuItem onClick={() => {}} label="My Reservations" />
+
+                {/* TODO Replace Menu item */}
+                <MenuItem onClick={() => {}} label="My Properties" />
+
+                {/* TODO Replace Menu item */}
+                <MenuItem onClick={() => {}} label="Register your home" />
+
+                <hr />
+
+                {/* Signout Menu item */}
+                <MenuItem onClick={() => signOut()} label="Sign out" />
+              </>
+            ) : (
+              <>
+                {/* Login Menu item */}
+                <MenuItem onClick={loginModal.onOpen} label="login" />
+
+                {/* Signup Menu item */}
+                <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
